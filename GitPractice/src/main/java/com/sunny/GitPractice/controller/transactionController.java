f3c2fe7d3f0503832d86d73c0e0ccbabbdaff9df
Prajyot <prajyot.jagtap@outlook.com>
2022-03-19 19:43:54 -0500

    First commit

--- GitPractice/src/main/java/com/sunny/GitPractice/controller/transactionController.java
+++ GitPractice/src/main/java/com/sunny/GitPractice/controller/transactionController.java
@@ -1,7 +1,14 @@
 package com.sunny.GitPractice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class transactionController {
+
+    @GetMapping("/home")
+    public String welcomeApi(){
+
+        return "Welcome to my website";
+    }
 }
